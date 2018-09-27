2bf7f6d703a5436aaf1f666b212b09fa9a7278ef
icywang86rui <icywang.rui@gmail.com>
2018-09-27 10:23:05 -0700

    Scriptmode single machine training implementation (#78)
    * Scriptmode with cpu docker py2 and py3 docker file
    
    * Migrate to sagemaker-containers 2.1
    * Remove serving related packages and code from container
    * Add py3 container
    * Add integ and unit tests for script mode
    * Remove non-asci characters from  README
    
    * Changes based on pr comments
    
    * Move conftest to test root dir
    
    * Add default values for test args
    
    * add docker-compose to test requirement

--- test/unit/test_training.py
+++ test/unit/test_training.py
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 # Copyright 2017-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+=======
+# Copyright 2017-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+>>>>>>> Scriptmode single machine training implementation (#78)
 #
 # Licensed under the Apache License, Version 2.0 (the "License"). You
 # may not use this file except in compliance with the License. A copy of
@@ -6,12 +10,18 @@
 #
 #     http://aws.amazon.com/apache2.0/
 #
+<<<<<<< HEAD
 # or in the 'license' file accompanying this file. This file is
 # distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+=======
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+>>>>>>> Scriptmode single machine training implementation (#78)
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 from __future__ import absolute_import
 
+<<<<<<< HEAD
 import os
 import sys
 
@@ -296,3 +306,28 @@ def test_main_tuning_mpi_model_dir(configure_s3_env, read_hyperparameters, train
     os.environ['SAGEMAKER_REGION'] = REGION
     training.main()
     configure_s3_env.assert_called_once_with('/opt/ml/model', REGION)
+=======
+import pytest
+from mock import MagicMock, patch
+
+from tf_container import training
+
+MODULE_DIR = 's3://my/bucket'
+MODULE_NAME = 'script_name'
+
+@pytest.fixture
+def single_machine_training_env():
+    env = MagicMock()
+
+    env.module_dir = MODULE_DIR
+    env.module_name = MODULE_NAME
+    env.hyperparameters = {}
+
+    return env
+
+@patch('sagemaker_containers.beta.framework.modules.run_module')
+def test_single_machine(run_module, single_machine_training_env):
+    training.train(single_machine_training_env)
+    run_module.assert_called_with(MODULE_DIR, single_machine_training_env.to_cmd_args(),
+                                  single_machine_training_env.to_env_vars(), MODULE_NAME)
+>>>>>>> Scriptmode single machine training implementation (#78)
