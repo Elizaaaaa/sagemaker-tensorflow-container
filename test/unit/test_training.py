323d2f954ae37e28754421a33aa32624337e9a5c
icywang86rui <icywang.rui@gmail.com>
2019-01-03 11:14:07 -0800

    Force parameter server to run on CPU (#143)

--- test/unit/test_training.py
+++ test/unit/test_training.py
@@ -328,6 +328,7 @@ import sys
 >>>>>>> Add distributed training support (#98)
 from mock import MagicMock, patch
 import pytest
+import tensorflow as tf
 
 from sagemaker_tensorflow_container import training
 
@@ -417,7 +418,9 @@ def test_train_distributed_master(run, tf_server, cluster_spec, distributed_trai
                                      'master': ['host1:2222'],
                                      'ps': ['host1:2223', 'host2:2223']})
 
-    tf_server.assert_called_with(cluster_spec(), job_name='ps', task_index=0)
+    tf_server.assert_called_with(
+        cluster_spec(), job_name='ps', task_index=0, config=tf.ConfigProto(device_count={'GPU': 0})
+    )
     tf_server().join.assert_called_with()
 
     tf_config = '{"cluster": {' \
@@ -447,7 +450,9 @@ def test_train_distributed_worker(run, tf_server, cluster_spec, distributed_trai
                                      'master': ['host1:2222'],
                                      'ps': ['host1:2223', 'host2:2223']})
 
-    tf_server.assert_called_with(cluster_spec(), job_name='ps', task_index=1)
+    tf_server.assert_called_with(
+        cluster_spec(), job_name='ps', task_index=1, config=tf.ConfigProto(device_count={'GPU': 0})
+    )
     tf_server().join.assert_called_with()
 
     tf_config = '{"cluster": {' \
