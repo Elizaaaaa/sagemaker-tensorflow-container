4d18645ba19b25d293bebe0f78562e570e2d3c29
icywang86rui <icywang.rui@gmail.com>
2019-01-25 15:12:59 -0800

    Add S3 plugin tests (#155)
    * Add S3 plugin tests
    
    TensorFlow's S3 plugin doesn't work well with S3's eventual consistency model so
    we have seen training job failing due to checkpoint or model exporting to S3.
    Recently we have released our prod containers with a S3 plugin patch. This
    should reduce or eliminate such errors.
    
    The Test added writes a checkpoint to S3 after every training step. It fails
    with vanilla TensorFlow.
    
    * Remove distributed_mnist.py
    
    * Fix line too long

--- test/integration/local/test_training.py
+++ test/integration/local/test_training.py
@@ -20,23 +20,12 @@ from sagemaker.tensorflow import TensorFlow
 
 from test.integration.utils import processor, py_version  # noqa: F401
 
-<<<<<<< HEAD
 RESOURCE_PATH = os.path.join(os.path.dirname(__file__), '..', '..', 'resources')
 TF_CHECKPOINT_FILES = ['graph.pbtxt', 'model.ckpt-0.index', 'model.ckpt-0.meta']
 
-<<<<<<< HEAD
+
 @pytest.fixture  # noqa: F811
 def py_full_version(py_version):  # noqa: F811
-=======
-=======
->>>>>>> Create parameter server in different thread (#129)
-RESOURCE_PATH = os.path.join(os.path.dirname(__file__), '..', '..', 'resources')
-TF_CHECKPOINT_FILES = ['graph.pbtxt', 'model.ckpt-0.index', 'model.ckpt-0.meta']
-
-
-@pytest.fixture
-def py_full_version(py_version):
->>>>>>> Add distributed training support (#98)
     if py_version == '2':
         return '2.7'
     else:
@@ -64,21 +53,14 @@ def test_py_versions(sagemaker_local_session, docker_image, py_full_version, fra
 
 
 @pytest.mark.skip_gpu
-<<<<<<< HEAD
 def test_mnist_cpu(sagemaker_local_session, docker_image, tmpdir, framework_version):
-=======
-def test_mnist_cpu(sagemaker_local_session, docker_image, tmpdir):
->>>>>>> Add distributed training support (#98)
     output_path = 'file://{}'.format(tmpdir)
     run_tf_training(script=os.path.join(RESOURCE_PATH, 'mnist', 'mnist.py'),
                     instance_type='local',
                     instance_count=1,
                     sagemaker_local_session=sagemaker_local_session,
                     docker_image=docker_image,
-<<<<<<< HEAD
                     framework_version=framework_version,
-=======
->>>>>>> Add distributed training support (#98)
                     output_path=output_path,
                     training_data_path='file://{}'.format(
                         os.path.join(RESOURCE_PATH, 'mnist', 'data')))
@@ -86,45 +68,29 @@ def test_mnist_cpu(sagemaker_local_session, docker_image, tmpdir):
 
 
 @pytest.mark.skip_cpu
-<<<<<<< HEAD
 def test_gpu(sagemaker_local_session, docker_image, framework_version):
-=======
-def test_gpu(sagemaker_local_session, docker_image):
->>>>>>> Add distributed training support (#98)
     run_tf_training(script=os.path.join(RESOURCE_PATH, 'gpu_device_placement.py'),
                     instance_type='local_gpu',
                     instance_count=1,
                     sagemaker_local_session=sagemaker_local_session,
                     docker_image=docker_image,
-<<<<<<< HEAD
                     framework_version=framework_version,
-=======
->>>>>>> Add distributed training support (#98)
                     training_data_path='file://{}'.format(
                         os.path.join(RESOURCE_PATH, 'mnist', 'data')))
 
 
 @pytest.mark.skip_gpu
-<<<<<<< HEAD
 def test_distributed_training_cpu_no_ps(sagemaker_local_session,
                                         docker_image,
                                         tmpdir,
                                         framework_version):
     output_path = 'file://{}'.format(tmpdir)
     run_tf_training(script=os.path.join(RESOURCE_PATH, 'mnist', 'mnist_estimator.py'),
-=======
-def test_distributed_training_cpu_no_ps(sagemaker_local_session, docker_image, tmpdir):
-    output_path = 'file://{}'.format(tmpdir)
-    run_tf_training(script=os.path.join(RESOURCE_PATH, 'mnist', 'distributed_mnist.py'),
->>>>>>> Add distributed training support (#98)
                     instance_type='local',
                     instance_count=2,
                     sagemaker_local_session=sagemaker_local_session,
                     docker_image=docker_image,
-<<<<<<< HEAD
                     framework_version=framework_version,
-=======
->>>>>>> Add distributed training support (#98)
                     output_path=output_path,
                     training_data_path='file://{}'.format(
                         os.path.join(RESOURCE_PATH, 'mnist', 'data-distributed')))
@@ -132,26 +98,17 @@ def test_distributed_training_cpu_no_ps(sagemaker_local_session, docker_image, t
 
 
 @pytest.mark.skip_gpu
-<<<<<<< HEAD
 def test_distributed_training_cpu_ps(sagemaker_local_session,
                                      docker_image,
                                      tmpdir,
                                      framework_version):
     output_path = 'file://{}'.format(tmpdir)
     run_tf_training(script=os.path.join(RESOURCE_PATH, 'mnist', 'mnist_estimator.py'),
-=======
-def test_distributed_training_cpu_ps(sagemaker_local_session, docker_image, tmpdir):
-    output_path = 'file://{}'.format(tmpdir)
-    run_tf_training(script=os.path.join(RESOURCE_PATH, 'mnist', 'distributed_mnist.py'),
->>>>>>> Add distributed training support (#98)
                     instance_type='local',
                     instance_count=2,
                     sagemaker_local_session=sagemaker_local_session,
                     docker_image=docker_image,
-<<<<<<< HEAD
                     framework_version=framework_version,
-=======
->>>>>>> Add distributed training support (#98)
                     output_path=output_path,
                     hyperparameters={'sagemaker_parameter_server_enabled': True},
                     training_data_path='file://{}'.format(
@@ -159,8 +116,6 @@ def test_distributed_training_cpu_ps(sagemaker_local_session, docker_image, tmpd
     _assert_files_exist_in_tar(output_path, TF_CHECKPOINT_FILES)
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 def run_tf_training(script,
                     instance_type,
                     instance_count,
@@ -185,59 +140,6 @@ def run_tf_training(script,
                            base_job_name='test-tf',
                            framework_version=framework_version,
                            py_version='py3')
-=======
-class ScriptModeTensorFlow(Framework):
-    """This class is temporary until the final version of Script Mode is released.
-    """
-
-    __framework_name__ = "tensorflow-scriptmode-beta"
-
-    create_model = TensorFlow.create_model
-
-    def __init__(self, py_version='py', **kwargs):
-        self.requirements_file = None
-        self.py_version = py_version
-        self.framework_version = 'some version'
-        super(ScriptModeTensorFlow, self).__init__(**kwargs)
-
-
-def run_tf_training(script, instance_type, instance_count,
-                    sagemaker_local_session,
-                    docker_image, training_data_path, output_path=None,
-                    hyperparameters={}):
-    estimator = ScriptModeTensorFlow(entry_point=script,
-                                     role='SageMakerRole',
-                                     train_instance_count=instance_count,
-                                     train_instance_type=instance_type,
-                                     sagemaker_session=sagemaker_local_session,
-                                     image_name=docker_image,
-                                     output_path=output_path,
-                                     hyperparameters=hyperparameters,
-                                     base_job_name='test-tf')
->>>>>>> Add distributed training support (#98)
-=======
-def run_tf_training(script,
-                    instance_type,
-                    instance_count,
-                    sagemaker_local_session,
-                    docker_image, training_data_path, output_path=None,
-                    hyperparameters=None):
-
-    hyperparameters = hyperparameters or {}
-
-    estimator = TensorFlow(entry_point=script,
-                           role='SageMakerRole',
-                           train_instance_count=instance_count,
-                           train_instance_type=instance_type,
-                           sagemaker_session=sagemaker_local_session,
-                           image_name=docker_image,
-                           model_dir='/opt/ml/model',
-                           output_path=output_path,
-                           hyperparameters=hyperparameters,
-                           base_job_name='test-tf',
-                           framework_version='1.11.0',
-                           py_version='py3')
->>>>>>> Create parameter server in different thread (#129)
 
     estimator.fit(training_data_path)
 
